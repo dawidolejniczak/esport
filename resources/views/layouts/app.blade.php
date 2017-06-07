d95504484a37e5d960cc422c0ef57c2b7cdee69f
Dawid <daw98olejniczak@gmail.com>
2017-06-07 03:23:41 +0200

    Share Post

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -102,6 +102,15 @@
         </div>
     </div>
 </header>
+<div id="fb-root"></div>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9&appId=1587551761269228";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
 @include('layouts.filters')
 @yield('content')
 @include('layouts.footer')
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,8 +3,11 @@
         <header>
             <h2><a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a></h2>
             <ul class="social">
-                <li><a href=""><img src="img/socials.jpg" alt=""></a></li>
-                <!--<li><a href="">facebook</a></li>
+                <div class="fb-share-button" data-href="{{ route('posts.show', $post->id) }}" data-layout="button_count" data-size="small" data-mobile-iframe="true">
+                    <a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">Share</a>
+                </div>
+                <!--<li><a href=""><img src="img/socials.jpg" alt=""></a></li>
+                <li><a href="">facebook</a></li>
                 <li><a href="">twitter</a></li>
                 <li><a href="">reddit</a></li>-->
             </ul>
