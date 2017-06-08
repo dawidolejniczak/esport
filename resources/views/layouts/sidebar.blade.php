38aa0770cb05c940afda136dc3c3ed96f966ecbf
Dawid <daw98olejniczak@gmail.com>
2017-06-08 22:21:16 +0200

    SideBar With Facebook Link

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,11 @@
 <aside class="col-md-4 col-sm-12 col-xs-12">
     <div class="box">
         <h2>Find us in social media</h2>
-        <a href=""><img src="/img/banner-fb.jpg" alt=""></a>
+        <div class="fb-page" data-href="https://www.facebook.com/facebook" data-small-header="false"
+             data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false">
+            <blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a
+                        href="https://www.facebook.com/facebook">Facebook</a></blockquote>
+        </div>
     </div>
     <div class="box">
         <a href="#"><img src="http://lorempixel.com/300/300/" alt=""></a>
