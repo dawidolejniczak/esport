ca9a2f7db034b24660bc9ce078cd05d6c15836bd
Dawid <daw98olejniczak@gmail.com>
2017-06-12 22:00:41 +0200

    Change format of images in database...

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email', 100)->unique();
             $table->string('password')->nullable();
-            $table->binary('image')->nullable();
+            $table->string('image')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
