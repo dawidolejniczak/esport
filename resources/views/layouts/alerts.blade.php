b02278ec1a30e916be2ebacf4fbf8bfb5df31208
Dawid <daw98olejniczak@gmail.com>
2017-06-14 16:47:16 +0200

    Fix alerts

--- resources/views/layouts/alerts.blade.php
+++ resources/views/layouts/alerts.blade.php
@@ -1,20 +1,24 @@
-@if (count($errors) > 0)
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+<div class="container-fluid">
+    <div class="col-sm-12">
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        @if(Session::has('message'))
+            <div class="alert alert-success">
+                {{ Session::get('message') }}
+            </div>
+        @endif
+        @if(Session::has('dropdown'))
+            <script>
+                var dropdown_class = '.' + '{{ Session::get('dropdown') }}';
+                $(dropdown_class).addClass('open');
+            </script>
+        @endif
     </div>
-@endif
-@if(Session::has('message'))
-    <div class="alert alert-success">
-        {{ Session::get('message') }}
-    </div>
-@endif
-@if(Session::has('dropdown'))
-    <script>
-        var dropdown_class = '.' + '{{ Session::get('dropdown') }}';
-        $(dropdown_class).addClass('open');
-    </script>
-@endif
+</div>
