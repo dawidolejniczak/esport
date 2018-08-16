ec92c6106bfdc2da726b53b2980bd2d94c831531
Dawid Olejniczak <olejniczak.dawid@onet.pl>
2018-08-16 12:28:22 +0200

    Move social scripts to assets

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,3 +1,4 @@
 require('./_bootstrap');
 require('./_imageUpload');
 require('./_global');
+require('./components/main');
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -112,40 +112,8 @@ Request::is('password/edit'))
         </div>
     </div>
 </header>
-<div id="fb-root"></div>
-<div id="fb-root"></div>
+<div id="fb-root" data-facebook-client-id="{{ env('FACEBOOK_CLIENT_ID') }}"></div>
 
-<!-- Facebook -->
-<script>
-    (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9&appId={{ env('FACEBOOK_CLIENT_ID') }}";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
-
-<!-- Twitter -->
-<script>
-    window.twttr = (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0],
-            t = window.twttr || {};
-        if (d.getElementById(id)) return t;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "https://platform.twitter.com/widgets.js";
-        fjs.parentNode.insertBefore(js, fjs);
-
-        t._e = [];
-        t.ready = function (f) {
-            t._e.push(f);
-        };
-
-        return t;
-    }(document, "script", "twitter-wjs"));
-</script>
 @if(!Request::is('posts/create') && !Request::is('myprofile'))
     @include('layouts.filters')
 @endif
